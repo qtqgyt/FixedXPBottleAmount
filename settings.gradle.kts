bad7fa344f929f4ef297644d4e6dd9c9bde85cbd
qtqgyt <104375741+qtqgyt@users.noreply.github.com>
2025-12-28 21:31:13 -0700

    Backport to 1.21.9, 1.21.8, 1.21.7, and 1.21.6.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,13 +8,13 @@ pluginManagement {
 
 plugins {
     id("fabric-loom") version "1.14-SNAPSHOT"
-    id("dev.kikugie.stonecutter") version "0.8-beta.3"
+    id("dev.kikugie.stonecutter") version "0.8.1-beta.2"
 }
 
 rootProject.name = "FixedXPBottleAmount"
 
 stonecutter {
     create(rootProject) {
-        versions("1.21.11", "1.21.10")
+        versions("1.21.11", "1.21.10", "1.21.9", "1.21.8", "1.21.7", "1.21.6")
     }
 }
